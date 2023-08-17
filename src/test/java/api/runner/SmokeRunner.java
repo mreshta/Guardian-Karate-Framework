d4133b4ddf3941a881e9f88ff9f777927eb38237
mreshta <mreshta10@gmail.com>
2023-08-17 08:44:14 -0700

    The parametrized maven plugin

--- src/test/java/api/runner/SmokeRunner.java
+++ src/test/java/api/runner/SmokeRunner.java
@@ -2,13 +2,13 @@ package api.runner;
 
 import com.intuit.karate.junit5.Karate;
 
-public class TestRunner {
+public class SmokeRunner {
 
 	@Karate.Test
 	public Karate runTest() {
 		// address for our feature files.
 		// add tags
-		return Karate.run("classpath:feature").tags("@EndToEnd14");
+		return Karate.run("classpath:feature").tags("@Smoke");
 	}
 
 }
